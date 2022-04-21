6de65c3973ef6ae78d60166b9e41a33656e2651e
gurusabarish <gurusabarisha@gmail.com>
2022-04-21 22:01:42 +0530

    initial commit

--- src/Pages/SignUp/index.js
+++ src/Pages/SignUp/index.js
@@ -3,29 +3,42 @@ import config from "../../config";
 
 import SignUpForm from "./Form";
 
-import { Box, Card, CardContent, Typography, Divider } from "@mui/material";
+import { Box, Card, CardContent, Typography, Divider, Grid } from "@mui/material";
 import { UserCircle } from "tabler-icons-react";
 
 const SignUp = () => {
   return (
-    <Card
-      sx={{
-        borderRadius: config.borderRadius,
-        minWidth: 450,
-        boxShadow: "0px 0px 15px rgba(0, 0, 0, 0.1)",
-        backgroundColor: config.secondaryColor,
-      }}
-    >
-      <CardContent>
-        <Typography variant="h5" align="center" sx={{ my: 2 }}>
-          <UserCircle size={48} strokeWidth={2} color={"black"} />
-        </Typography>
-        <Divider>SignUp</Divider>
-        <Box sx={{ my: 2, px: 2 }}>
-          <SignUpForm />
-        </Box>
-      </CardContent>
-    </Card>
+    
+       <Grid
+        container
+        spacing={2}
+        alignItems="center"
+        justifyContent="center"
+        sx={{ minHeight: "100vh", backgroundColor: config.backgroundColor }}
+      >
+        <Grid item sx={{ mb: 3 }} xs={11} sm={6}>
+          <Card
+            sx={{
+              borderRadius: config.borderRadius,
+              boxShadow: "0px 0px 15px rgba(0, 0, 0, 0.1)",
+              backgroundColor: config.secondaryColor,
+              p: 2
+            }}
+          >
+            <SignUpForm />
+          </Card>
+        </Grid>
+      </Grid>
+    //   {/* <CardContent>
+    //     <Typography variant="h5" align="center" sx={{ my: 2 }}>
+    //       <UserCircle size={48} strokeWidth={2} color={"black"} />
+    //     </Typography>
+    //     <Divider>SignUp</Divider>
+    //     <Box sx={{ my: 2, px: 2 }}>
+    //       <SignUpForm />
+    //     </Box>
+    //   </CardContent> */}
+    // // </Card>
   );
 };
 
