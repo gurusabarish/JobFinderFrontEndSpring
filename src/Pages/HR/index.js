61f13fcdeaeac544ceb202af563bd999b0aec668
gurusabarish <gurusabarisha@gmail.com>
2022-04-24 15:35:26 +0530

    job create

--- src/Pages/HR/index.js
+++ src/Pages/HR/index.js
@@ -1,11 +1,33 @@
 import React from "react";
+import axios from "axios";
+import { Box, Grid } from "@mui/material";
 
-const HR = () => {
+import MainCard from "../../Components/MainCard";
+import CreateJob from "./Job";
+
+const HR = (props) => {
   return (
     <>
-      <h1>HR</h1>
+      <Grid
+        container
+        spacing={2}
+        // alignItems="center"
+        justifyContent="center"
+        py={2}
+        justifyItems={"center"}
+      >
+        <Grid item xs={12} sm={6} p={2}>
+          <MainCard title="Jobs">
+            <CreateJob user={props.user} />
+            <Box my={3}></Box>
+          </MainCard>
+        </Grid>
+        <Grid item xs={12} sm={6} p={2}>
+          <MainCard title="Application list"></MainCard>
+        </Grid>
+      </Grid>
     </>
   );
-};
+}; 
 
 export default HR;
